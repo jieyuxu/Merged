ad4e8ec9206da7b35b3a626c9d0cf635bd237b83
hhzrabbit <hzeng@stuy.edu>
2016-05-17 10:03:59 -0400

    created Course.java
    added constructor

--- Course.java
+++ Course.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList; 
+
 public class Course {
-
-
-
-
+  
+  private String _name, _department, _description;
+  private ArrayList<Student> _roster;
+  
+public Course(String name, String department, String description){
+  _name = name;
+  _department = department
+  _description = description
+  _roster = new ArrayList<Student>(); 
+}
 
 }
