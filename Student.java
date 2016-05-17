2e82306b0b132d9c91808caeaaf3f2705f7e2e9d
jieyuxu <jxu9@stuy.edu>
2016-05-17 09:55:24 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,7 @@
-
+public class Student{
+  //Private vars: osis, name, grade (2D array), gpa (calcGPA fxn), ap slots
+  private int _osis, _apslots;
+  private double _gpa;
+  private String _name;
+  private int[][] _grades;
+}
