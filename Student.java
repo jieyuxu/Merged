51c595f923a119cce630b164252905c8e3bbf00d
c-breen <cbreen@stuy.edu>
2016-05-17 10:03:33 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -5,9 +5,26 @@ public class Student{
   private String _name;
   private int[][] _grades;
   
-  public Student(int osis, int apslots, double gpa, String name, int[][] grades){
-    
-    
+  public Student(int osis, int apslots, String name, int[][] grades){
+    _osis = osis;
+    _apslots = apslots;
+    _name = name;
+    _grades = grades;
+    _gpa = getGpa();
   }
+  
+  private double getGpa(){
+    int total = 0;
+    int numClasses = 0;
+    for (int i = 0; i < grades.length; i++){
+      for (int j = 0; j < grades[i].length; j++){
+        total += grades[i][j];
+        numClasses++;
+      }
+    }
+    return total * 1.0 / numClasses;
   }
+  
+  
+  
 }
