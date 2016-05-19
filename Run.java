e9358e77ffe47c5215f22aaddfaaf3a2e64aefe2
hhzrabbit <hzeng@stuy.edu>
2016-05-18 23:01:05 -0400

    Update Run.java
    first attempt at using csv reading and parsing

--- Run.java
+++ Run.java
@@ -1,8 +1,21 @@
 public class Run {
+  
+  import java.io.BufferedReader;
+  import java.io.FileNotFoundException;
+  import java.io.FileReader;
+  import java.io.IOException;
 
-  public static void main(String [] args){
-    String csvName = "programrequests.csv"
-    
+  public static ArrayList<String[]> getListOfStudentsFromCSV(csvName){
+    ArrayList<String[]> students = new ArrayList<String[]>();
+    br = new BufferedReader(new FileReader(csvName));
+		while ((line = br.readLine()) != null) {
+		  students.add(line.split",");
+		}
+		return students;
   }
 
+  public static void main(String [] args){
+    ArrayList<String> a = getListOfStudentsFromCSV("programrequests.csv")
+    print a;
+  }
 }
