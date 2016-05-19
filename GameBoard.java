80a79264a4a9fef5c55a7dce9da762be783a4862
jieyuxu <jxu9@stuy.edu>
2016-05-19 10:15:56 -0400

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -1,3 +1,16 @@
 public class GameBoard{
+  private int _spots;
+  private Tile[][] _board;
+
+  
+  public GameBoard(){
+    _board = new Tile[5][5]; //always a square matrix
+    _spots = _board.length() * _board.length();
+  }
+  
+  public int getSpots(){
+    return _spots;
+  }
+  
 
 }
