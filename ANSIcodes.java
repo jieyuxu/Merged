d342e2cb9ae38b8d9c30950b32d7dae5f9865b1c
hhzrabbit <hzeng@stuy.edu>
2016-05-19 16:30:49 -0400

    Update ANSIcodes.java
    comments with numbers

--- ANSIcodes.java
+++ ANSIcodes.java
@@ -1,9 +1,10 @@
-public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_RESET = "\u001B[0m"; 
 public static final String ANSI_BLACK = "\u001B[30m";
-public static final String ANSI_RED = "\u001B[31m";
-public static final String ANSI_GREEN = "\u001B[32m";
-public static final String ANSI_YELLOW = "\u001B[33m";
-public static final String ANSI_BLUE = "\u001B[34m";
-public static final String ANSI_PURPLE = "\u001B[35m";
-public static final String ANSI_CYAN = "\u001B[36m";    
+public static final String ANSI_RED = "\u001B[31m";  //3
+public static final String ANSI_GREEN = "\u001B[32m";  //5
+public static final String ANSI_YELLOW = "\u001B[33m";  //6
+public static final String ANSI_BLUE = "\u001B[34m";  //4
+public static final String ANSI_PURPLE = "\u001B[35m"; //1
+public static final String ANSI_CYAN = "\u001B[36m";    //2
 public static final String ANSI_WHITE = "\u001B[37m";
+public static final String ANSI_BLACKBACKGROUND = "\u001B[40m";  //7
