08bca0da349ad4eca3a5bb84c97f4ca9efeb6601
c-breen <cbreen@stuy.edu>
2016-05-19 09:56:32 -0400

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,7 +1,26 @@
 public class Tile {
 
-  private int _num;
-  priavte String _colorCode;
+  private int _val;
+  private String _colorCode;
   private Tile _neighbor;
+  public static final String ANSI_RESET =  "\u001B[0m";
 
-  public Tile(int num, String colorCode, Tile neighbor){
+  public Tile(int val, String colorCode, Tile neighbor){
+    _val = val;
+    _colorCode = colorCode;
+    _neighbor = neighbor;
+  }
+  
+  public int getVal(){
+    return _val;
+  }
+  
+  public Tile getNeighbor(){
+    return _neighbor;
+  }
+  
+  public String toString(){
+    return _colorCode + " " + getVal() + ANSI_RESET;
+  }
+  
+}
