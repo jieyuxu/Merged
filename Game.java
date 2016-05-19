d27be9c9e73d8d5b4471dc62ea5d8e87bbd629d5
Haley Zeng <hzeng@stuy.edu>
2016-05-19 15:52:30 -0400

    creation of 2 distinct tiles

--- Game.java
+++ Game.java
@@ -1,35 +1,49 @@
-public Class Game {
+import java.util.ArrayList;
+
+public class Game {
     
     private GameBoard _board;
     private int _score;
     
     public Game(){
-        _board = new GameBoard();    
+	//   _board = new GameBoard();    
         _score = 0;
     }
 
-    public Tile genOneTile(){
-        int maxVal = Board.maxVal;
-        int tileVal = (int) (Math.random() * maxVal + 1);
+    public Tile genOneTile(ArrayList<Integer> options){
+        int choice = (int) (Math.random() * options.size());
+	int tileVal = options.get(choice);
         return new Tile(tileVal, null);
-    
     }
 
-    public Tile getNextTile(){
+    public Tile getTwoTiles(ArrayList<Integer> options){
+	Tile t1 = genOneTile(options);
+	// t1.getVal returns Integer Object
+	// uses remove(Object o)
+	options.remove(t1.getVal());
+	Tile t2 = genOneTile(options);
+	return t2;
+    }
+    
+    /*   public Tile getNextTile(){
         int howManyTiles = 1;
         if (Board.fitTwo())
             howManyTiles = (int) (Math.random() * 2 + 1);   
             
     
     }
-
+    */
     public void play(){
         System.out.println(_board);
     }
 
     public static void main(String [] args){
         Game game = new Game();
-        game.play();
+	//  game.play();
+	ArrayList<Integer> a = new ArrayList<Integer>();
+	for (int i = 1; i < 10; i++)
+	    a.add(i);
+	game.getTwoTiles(a);
     }
 
 }
