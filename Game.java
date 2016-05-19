8bbf8c1e0479bbbc0955d6bda91a0f7ea5d29baf
hhzrabbit <hzeng@stuy.edu>
2016-05-19 10:01:02 -0400

    Update Game.java
    tile generation

--- Game.java
+++ Game.java
@@ -1,8 +1,27 @@
 public Class Game {
     
+    Board _board;
+    int _score;
     
-  
+    public Game(){
+        _board = new Board();    
+        _score = 0;
+    }
 
+    public Tile genNextTile(){
+        int maxVal = Board.maxVal;
+        int tileVal = (int) (Math.random() * maxVal + 1);
+        return new Tile(tileVal);
+    
+    }
 
+    public void play(){
+        
+    }
+
+    public static void main(String [] args){
+        Game game = new Game();
+        game.play();
+    }
 
 }
