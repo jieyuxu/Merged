83f9be06b74780bca27ee90c1a4a03390d34983f
Haley Zeng <hzeng@stuy.edu>
2016-05-19 16:30:13 -0400

    7 has black backgroun

--- Tile.java
+++ Tile.java
@@ -1,36 +1,49 @@
 public class Tile {
 
-  private Integer _val;
-  private String _colorCode;
-  private Tile _neighbor;
-  private int _orientation; //can only be 0 (up), 1 (left), 2 (down), 3 (right)
-  private String[] _codes = {"\u001B[35m", "\u001B[36m", "\u001B[31m", "\u001B[34m", "\u001B[32m", "\u001B[33m"};
-  public static final String RESET =  "\u001B[0m";
+    private Integer _val;
+    private String _colorCode;
+    private Tile _neighbor;
+    private int _orientation; //can only be 0 (up), 1 (left), 2 (down), 3 (right)
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_BLACKBACKGROUND = "\u001B[40m";
+    private String[] _codes = {ANSI_PURPLE, ANSI_CYAN, ANSI_RED, ANSI_BLUE, ANSI_GREEN, ANSI_YELLOW, ANSI_BLACKBACKGROUND};
+    public static final String RESET =  "\u001B[0m";
   
 
-  public Tile(Integer val, Tile neighbor){
-    _val = val;
-    _colorCode = _codes[val-1];
-    _neighbor = neighbor;
-  }
+    public Tile(Integer val, Tile neighbor){
+	_val = val;
+	_colorCode = _codes[val-1];
+	_neighbor = neighbor;
+    }
   
-  public Integer getVal(){
-    return _val;
-  }
+    public Integer getVal(){
+	return _val;
+    }
   
-  public Tile getNeighbor(){
-    return _neighbor;
-  }
+    public Tile getNeighbor(){
+	return _neighbor;
+    }
   
-  public String toString(){
-    return _colorCode + getVal() + RESET;
-  }
+    public String toString(){
+	return _colorCode + getVal() + RESET;
+    }
       
-  public static void main(String [] args){
-	  Tile x = new Tile(3, null);
-	  Tile y = new Tile(4, x);
-  	System.out.println(x);
-	  System.out.println(y);
+    public static void main(String [] args){
+	//	Tile x = new Tile(3, null);
+	//	Tile y = new Tile(4, x);
+	//	System.out.println(x);
+	//	System.out.println(y);
+	for (int i = 1; i <= 7; i++) {
+	    Tile b = new Tile(i, null);
+	    System.out.println(b);
+	}
     }
   
 }
