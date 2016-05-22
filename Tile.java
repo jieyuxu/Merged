72aeafd987234b788f96316507de9340b11722b3
Haley Zeng <hzeng@stuy.edu>
2016-05-22 14:32:29 -0400

    7: white text

--- Tile.java
+++ Tile.java
@@ -5,14 +5,14 @@ public class Tile {
     private Tile _neighbor;
     private int _orientation; //can only be 0 (up), 1 (left), 2 (down), 3 (right)
 
-    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RESET = "\u001B[0m"; //regular text
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_BLACKBACKGROUND = "\u001B[40m";
+    public static final String ANSI_BLACKBACKGROUND = "\u001B[37;40m";
     private String[] _codes = {ANSI_PURPLE, ANSI_CYAN, ANSI_RED, ANSI_BLUE, ANSI_GREEN, ANSI_YELLOW, ANSI_BLACKBACKGROUND};
     public static final String RESET =  "\u001B[0m";
   
