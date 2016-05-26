5523b5971419dcfe1cc5d791be2f0a9ba620ea1a
hhzrabbit <hzeng@stuy.edu>
2016-05-26 10:07:04 -0400

    Update Game.java
    fsogiv github sucks

--- Game.java
+++ Game.java
@@ -12,29 +12,39 @@ public class Game {
 
     public Tile genOneTile(ArrayList<Integer> valOptions, Tile neighbor){
         int choice = (int) (Math.random() * valOptions.size());
-	System.out.println(choice + " first");			   
 	int tileVal = valOptions.get(choice);
-	System.out.println(tileVal);
         return new Tile(tileVal, neighbor);
     }
 
-    public Tile getTwoTiles(ArrayList<Integer> valOptions){
+    public Tile genTwoTiles(ArrayList<Integer> valOptions){
 	Tile t1 = genOneTile(valOptions, null);
-	// t1.getVal returns Integer Object
+	// t1.getVal() returns Integer Object
 	// uses remove(Object o)
 	valOptions.remove(t1.getVal());
 	Tile t2 = genOneTile(valOptions, t1);
+	t1.setNeighbor(t2);
 	return t1;
     }
     
-    /*   public Tile getNextTile(){
-        int howManyTiles = 1;
-        if (Board.fitTwo())
-            howManyTiles = (int) (Math.random() * 2 + 1);   
-            
-    
+    public Tile getNextPiece(){
+	ArrayList<Integer> possible = new ArrayList<Integer>();
+	for (int i = 1; i <= 7; i++)
+	    possible.add(i);
+	Tile piece = genOneTile(possible, null);
+        if (_board.canFit2())
+	    if ((int) (Math.random() * 2) == 0){
+		piece = genTwoTiles(possible);
+	    }
+	return piece;
     }
-    */
+
+    public void rotatePiece(Tile t){
+	if (t.getNeighbor() == null) return;
+	t.setOrientation( (t.getOrientationX() - 1) % 2, (t.getOrientationY() - 1) % 2);
+	Tile n = t.getNeighbor();
+	n.setOrientation( (n.getOrientationY() - 1) % 2, (n.getOrientationY() - 1) % 2);
+    }
+
     public void play(){
         System.out.println(_board);
     }
