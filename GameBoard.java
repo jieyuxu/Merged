e11a472e96328a2baecc6f60064b25196f9c0c24
jieyuxu <jxu9@stuy.edu>
2016-05-26 10:04:03 -0400

    added canFitHere

--- GameBoard.java
+++ GameBoard.java
@@ -1,11 +1,13 @@
 public class GameBoard{
   private int _openSpots;
   private Tile[][] _board;
+  private int _maxVal;
 
   
   public GameBoard(){
     _board = new Tile[5][5]; //always a square matrix
     _openSpots = _board.length * _board.length;
+    _maxVal = 0;
   }
   
   public int numOpenSpots(){
@@ -15,7 +17,27 @@ public class GameBoard{
   public void add(int x, int y, Tile addin){
   	//if (_board[x][y] != null) return;
   	_board[x][y] = addin;
+    if (addin.getVal() > _maxVal)
+      _maxVal = addin.getVal();
   }
+
+    public boolean canFit2(){
+    	if (numOpenSpots() < 2) return false;
+            int maxIndex = _board.length - 1;
+      	for (int i = 0; i < maxIndex; i++){
+    	    for (int j = 0; j < maxIndex; j++){
+    		    if (_board[i][j] == null)
+    		    //check against right neighbor and downstairs neighbor
+    		    if (_board[i][j+1] == null || _board[i+1][j] == null) return true;
+    	    }
+      	}
+    	for (int i = 0; i < maxIndex; i++){
+    	    //check in last row against right neighbor
+    	    if (_board[maxIndex][i] == null && _board[maxIndex][i+1] == null) return true;
+    	    //check in last column against downstairs neighbor
+    	    if (_board[i][maxIndex] == null && _board[i+1][maxIndex] == null) return true;
+    	}
+      	return false;
 
     //public static boolean canFit2(String[][] _board){
     public boolean canFit2(){
@@ -35,7 +57,19 @@ public class GameBoard{
 	    if (_board[i][maxIndex] == null && _board[i+1][maxIndex] == null) return true;
 	}
   	return false;
+
     }
+
+  public static boolean canFitHere(int x, int y, Tile addin, Tile[][] board){
+       //if (! canFit2()) return false;
+       if (board[x][y] != null) return false;
+       int newX, newY;
+       newX = x + addin.getOrientationX();
+       newY = y + addin.getOrientationY();
+       if (newX >= board.length || newY >= board.length)
+          return false;
+       return board[newX][newY] == null;
+  }
 
   public String toString(){
     String ans = "";
@@ -59,10 +93,15 @@ public class GameBoard{
     for (int i = 0; i < len; i++)
     	for (int j = 0; j < len; j++)
     		test.add(i,j,new Tile(i+1, null));
+    
+    //test for canFit2()
+    test._board[1][0] = null;
     test._board[0][0] = null;
-    test._board[0][1] = null;
+    // test._board[0][1] = null;
+    // test._board[2][0] = null;
     System.out.println(test);
-    System.out.println(test.canFit2());
+    System.out.println(canFitHere(0,0, a, test._board));
+   // System.out.println(test.canFit2());
   }
 
 
