605847d5fe00b0801ff3973e6495cad42b8b61d9
c-breen <cbreen@stuy.edu>
2016-05-27 10:13:03 -0400

    canFitHere works!

--- GameBoard.java
+++ GameBoard.java
@@ -28,47 +28,34 @@ public class GameBoard{
     	    for (int j = 0; j < maxIndex; j++){
     		    if (_board[i][j] == null)
     		    //check against right neighbor and downstairs neighbor
-    		    if (_board[i][j+1] == null || _board[i+1][j] == null) return true;
+    		    if (_board[i][j+1] == null || _board[i+1][j] == null)
+			return true;
     	    }
       	}
     	for (int i = 0; i < maxIndex; i++){
     	    //check in last row against right neighbor
-    	    if (_board[maxIndex][i] == null && _board[maxIndex][i+1] == null) return true;
+    	    if (_board[maxIndex][i] == null && _board[maxIndex][i+1] == null)
+		return true;
     	    //check in last column against downstairs neighbor
-    	    if (_board[i][maxIndex] == null && _board[i+1][maxIndex] == null) return true;
+    	    if (_board[i][maxIndex] == null && _board[i+1][maxIndex] == null)
+		return true;
     	}
       	return false;
-
-    //public static boolean canFit2(String[][] _board){
-    public boolean canFit2(){
-	if (numOpenSpots() < 2) return false;
-        int maxIndex = _board.length - 1;
-  	for (int i = 0; i < maxIndex; i++){
-	    for (int j = 0; j < maxIndex; j++){
-		if (_board[i][j] == null)
-		    //check against right neighbor and downstairs neighbor
-		    if (_board[i][j+1] == null || _board[i+1][j] == null) return true;
-	    }
-  	}
-	for (int i = 0; i < maxIndex; i++){
-	    //check in last row against right neighbor
-	    if (_board[maxIndex][i] == null && _board[maxIndex][i+1] == null) return true;
-	    //check in last column against downstairs neighbor
-	    if (_board[i][maxIndex] == null && _board[i+1][maxIndex] == null) return true;
-	}
-  	return false;
-
     }
 
-  public static boolean canFitHere(int x, int y, Tile addin, Tile[][] board){
+
+  public static boolean canFitHere(int r, int c, Tile addin, Tile[][] board){
        //if (! canFit2()) return false;
-       if (board[x][y] != null) return false;
-       int newX, newY;
-       newX = x + addin.getOrientationX();
-       newY = y + addin.getOrientationY();
-       if (newX >= board.length || newY >= board.length)
+       if (board[r][c] != null) return false;
+       int newR, newC;
+       newR = r + addin.getOrientationR();
+       System.out.println(newR);
+       newC = c + addin.getOrientationC();
+       System.out.println(newC);
+       if (newR >= board.length || newC >= board.length ||
+	   newR < 0 || newC < 0)
           return false;
-       return board[newX][newY] == null;
+       return board[newR][newC] == null;
   }
 
   public String toString(){
@@ -88,19 +75,20 @@ public class GameBoard{
   public static void main(String[] args){
     GameBoard test = new GameBoard();
     Tile a = new Tile(2, null);
+    a.setOrientation(1,0);
     int len = test._board.length;
-    test.add(0,0,a);
+    //test.add(0,0,a);
     for (int i = 0; i < len; i++)
     	for (int j = 0; j < len; j++)
     		test.add(i,j,new Tile(i+1, null));
     
     //test for canFit2()
-    test._board[1][0] = null;
-    test._board[0][0] = null;
+    test._board[2][0] = null;
+    test._board[3][0] = null;
     // test._board[0][1] = null;
     // test._board[2][0] = null;
     System.out.println(test);
-    System.out.println(canFitHere(0,0, a, test._board));
+    System.out.println(canFitHere(2,0, a, test._board));
    // System.out.println(test.canFit2());
   }
 
