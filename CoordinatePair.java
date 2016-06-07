98a669b6defe8c997a40d777b363b001fd77efec
hhzrabbit <hzeng@stuy.edu>
2016-06-07 19:51:44 -0400

    Add files via upload
    cool man

---
+++ CoordinatePair.java
@@ -0,0 +1,19 @@
+public class CoordinatePair {
+
+    private int _row;
+    private int _col;
+
+    public CoordinatePair(int r, int c){
+	_row = r;
+	_col = c;
+    }
+
+    public int getRow(){
+	return _row;
+    }
+
+    public int getCol(){
+	return _col;
+    }
+
+}
